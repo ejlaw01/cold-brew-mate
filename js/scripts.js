d5840ece16da578272d40fddc93830b17461190d
Ethan Law <ejlaw01@gmail.com>
2017-01-17 16:51:30 -0800

    add functionality and scroll animation to navbar links

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+
+  var viewHeight = parseInt($(window).height());
+
   $(window).scroll(function () {
-    var viewHeight = parseInt($(window).height());
     console.log($(window).scrollTop())
     if ($(window).scrollTop() > (viewHeight * .9)) {
       $('.nav').addClass('navbar-fixed');
@@ -14,8 +16,13 @@ $(document).ready(function() {
     if (($(window).scrollTop() < 1365) || ($(window).scrollTop() > 2430)) {
       $('#water').removeClass('flow');
     }
-    // if ($(window).scrollTop() > 1800) {
-    //   $('#bucket-back').addClass('full');
-    // }
   });
+
+  $(document).on('click', 'a', function(event){
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top-(viewHeight*.1 - 1)
+    }, 400);
+});
+
 });
